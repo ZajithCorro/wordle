f8900a3757a5cc65145bcf7d7408a4ca4fec29f0
Zajith Corro <zajithcorrov@hotmail.com>
2022-06-12 21:39:02 -0500

    :sparkles: feat: delete a letter

--- src/components/word.js
+++ src/components/word.js
@@ -5,8 +5,11 @@ let currentIndexLetter = 1;
 function pushLetter(letter) {
 	const key = letter.toLowerCase();
 
+	const isBackspace = key === 'backspace';
 	const isLetter = validLetters.includes(key);
 	const canAddLetter = currentIndexLetter <= 5;
+
+	isBackspace && removeLetter();
 
 	if (isLetter && canAddLetter) addLeter(key);
 }
@@ -18,6 +21,18 @@ function addLeter(letter) {
 	currentLetter.classList.add('pop');
 	currentLetter.textContent = letter;
 	currentIndexLetter++;
+}
+
+function removeLetter() {
+	if (currentIndexLetter <= 1) return;
+
+	const lastIndexLetter = currentIndexLetter - 1;
+	const currentWord = document.querySelector('div[data-current-word]');
+	const currentLetter = currentWord.querySelector(`span:nth-child(${lastIndexLetter})`);
+
+	currentLetter.classList.remove('pop');
+	currentLetter.textContent = '';
+	currentIndexLetter--;
 }
 
 export { pushLetter };
